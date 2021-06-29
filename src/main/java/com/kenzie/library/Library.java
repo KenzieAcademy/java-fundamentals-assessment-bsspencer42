ff5d5aa8ca81d3a3b4ff1cc2c9b5c72c874dab23
bsspencer42 <bsspencer42@gmail.com>
2021-06-28 20:13:34 -0500

    Add checkout book method

--- src/main/java/com/kenzie/library/Library.java
+++ src/main/java/com/kenzie/library/Library.java
@@ -11,11 +11,49 @@ public class Library {
 	public Library() {
 		this.myLibrary = new ArrayList<Book>();
 		this.checkedOut = new ArrayList<String>();
+		
 	}
 	
 	// Add book method
 	public void addBook(Book newBook) {
 		myLibrary.add(newBook);
 	}
-
+	
+	// List all books
+	public String[] listBooks() {
+		// New local variable for string
+		String[] allBooks = new String[myLibrary.size()];
+		for (int i = 0; i < myLibrary.size(); i++) {
+			allBooks[i] = myLibrary.get(i).getTitle();
+		}
+		return allBooks;
+	}
+	
+	
+	
+	
+	
+	// Checkout book
+	public Book checkoutBook(String checkoutTitle) {
+		// Search the current inventory and check out book if available
+		for (int i = 0; i < myLibrary.size(); i++) {
+			// Local variable for current iteration title
+			String currentBook = myLibrary.get(i).getTitle();
+			
+				// Check if current book is the desired book
+				if (currentBook == checkoutTitle && !checkedOut.contains(currentBook)) {
+					// Add title to checkedOut array
+					checkedOut.add(currentBook);
+					// Finally, return book object
+					return myLibrary.get(i);
+				}
+				// If book checked out, throw exception
+				else if (currentBook == checkoutTitle && checkedOut.contains(currentBook)) {
+					throw new BookNotAvailableException(checkoutTitle);
+				}
+			}
+			// If book not found, throw not found exception
+			throw new BookNotFoundException(checkoutTitle);
+	}
+	
 }
--- src/main/java/com/kenzie/library/TestCases.java
+++ src/main/java/com/kenzie/library/TestCases.java
@@ -6,23 +6,23 @@ public class TestCases {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String [] myContents = { "This is page one.", "Love", "Reading" };
+		
+		// My book
+		String [] myContents = { "This is page one.", "This is page two", "Roses are red", "Tohmy is a Who"};
 		Book myBook = new Book("Sorcerer's Stone","J.K. Rowling",Genre.SCIENCE_FICTION, myContents);
-		
-		// Test case for private variable
-		//System.out.println(myBook.getNumPages());
-		System.out.println(myBook.openPage(3));
-		//System.out.println(myBook.getNumPages());
-		//System.out.println(myBook.tearOutCurrentPage());
-		System.out.println(myBook.getCurrentPageNumber());
-		System.out.println(myBook.openPage(2));
-		//System.out.println(myBook.nextPage());
-		System.out.println(myBook.getPercentageComplete());
-		
-		System.out.println(myBook.getGenre());
+		String [] myContents2 = { "Harry Potter", "Hermione", "Ronald Wealsey", "Voldemort"};
+		Book myBook2 = new Book("Chamber of Secrets","J.K. Rowling",Genre.SCIENCE_FICTION, myContents2);
 		
 		Library myLibrary = new Library();
 		myLibrary.addBook(myBook);
+		myLibrary.addBook(myBook2);
+		Book myBook3
+		= myLibrary.checkoutBook("Sorcerer's Stone");
+		//myBook2.tearOutCurrentPage();
+		//Book myBook3 = myLibrary.checkoutBook("Sorcerer's Stone");
+		for (String book : myLibrary.listBooks()) {
+			System.out.println(book);
+		}
 		
 		
 		// Test script for next/prev page
